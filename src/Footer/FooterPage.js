4d17dd66beabb832cc7f7251d1d1bcf210234335
Myosotis <dnlgmldnjs123123@gmail.com>
2024-11-25 13:02:04 +0900

    Add all Lambda functions

--- src/Footer/FooterPage.js
+++
@@ -1,9 +0,0 @@
-function FooterPage() {
-    return (
-        <main className="footer-page">
-                <div className="footer-logo"/>
-        </main>
-    )
-}
-
-export default FooterPage;
